0afb81d2402e136be2d3835b64767de37e84adcf
AlehZakharau <Alehzaharau@tutanota.com>
2022-01-22 04:13:23 -0500

    Start Up

---
+++ Assets/Scripts/Ecs/EcsStartUp.cs
@@ -0,0 +1,22 @@
+﻿using Leopotam.Ecs;
+using UnityEngine;
+using VContainer.Unity;
+
+namespace Ecs
+{
+    public class EcsStartUp : IStartable
+    {
+        private readonly EcsWorld world;
+        private EcsSystems systems;
+        
+        public EcsStartUp(EcsWorld world)
+        {
+            this.world = world;
+        }
+        public void Start()
+        {
+            systems = new EcsSystems(world);
+            Debug.Log($"CreateWorld {world.IsAlive().ToString()}");
+        }
+    }
+}
---
+++ Assets/Scripts/Ecs/GameLifeTimeScope.cs
@@ -0,0 +1,32 @@
+﻿using Leopotam.Ecs;
+using UnityEngine;
+using UnityTemplateProjects;
+using VContainer;
+using VContainer.Unity;
+
+namespace Ecs
+{
+    public class GameLifeTimeScope : LifetimeScope
+    {
+        [SerializeField] private DataView dataView;
+        protected override void Configure(IContainerBuilder builder)
+        {
+            RegisterWorld(builder);
+            builder.RegisterEntryPoint<DataController>();
+            builder.RegisterComponent(dataView);
+            base.Configure(builder);
+        }
+
+
+        private void RegisterWorld(IContainerBuilder builder)
+        {
+            builder.RegisterEntryPoint<EcsStartUp>();
+            builder.Register<EcsWorld>(x =>
+            {
+                var world = new EcsWorld();
+
+                return world;
+            }, Lifetime.Singleton);
+        }
+    }
+}
