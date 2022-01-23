859190c7718ad73053fc6337a8ef174260141124
AlehZakharau <Alehzaharau@tutanota.com>
2022-01-23 17:53:31 -0500

    Camera Scripts

---
+++ Assets/Scripts/Camera/MouseClicker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityTemplateProjects.UnitySystem;
+
+namespace UnityTemplateProjects
+{
+    [RequireComponent(typeof(Camera))]
+    public sealed class MouseClicker : MonoBehaviour
+    {
+        private Camera camera;
+        
+        private IClickable currentObject;
+
+        private int clicableMask;
+
+        private void Start()
+        {
+            camera = GetComponent<Camera>();
+            clicableMask = LayerMask.GetMask("Manufacture");
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                currentObject?.UnSelect();
+                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, clicableMask))
+                {
+                    if (hit.collider.TryGetComponent(out IClickable clickable))
+                    {
+                        currentObject = clickable;
+                        currentObject.Select();
+                        currentObject.Click();
+                    };
+                }
+            }
+        }
+    }
+}
