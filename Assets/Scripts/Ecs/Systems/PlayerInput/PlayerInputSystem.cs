f268884ad31075139bd1c8a0b971dd808facf458
AlehZakharau <Alehzaharau@tutanota.com>
2022-02-14 20:52:24 -0500

    Clickable Ecs

--- Assets/Scripts/Ecs/Systems/PlayerInput/PlayerInputSystem.cs
+++ Assets/Scripts/Ecs/Systems/PlayerInput/PlayerInputSystem.cs
@@ -1,5 +1,9 @@
-﻿using Leopotam.Ecs;
+﻿using CameraService;
+using Ecs.Components;
+using Ecs.View.Impl;
+using Leopotam.Ecs;
 using PlayerInput;
+using UnityEditor;
 using UnityEngine;
 
 namespace Ecs.PlayerInput
@@ -7,9 +11,14 @@ namespace Ecs.PlayerInput
     public class PlayerInputSystem : IEcsInitSystem, IEcsRunSystem
     {
         private readonly Controls controls = null;
+        private readonly EcsFilter<CameraComponent, LinkComponent> camera = default;
+
+        private IClickable currentClickable;
+        private int clickableMask;
         public void Init()
         {
             controls.Enable();
+            clickableMask = LayerMask.GetMask("Manufacture");
         }
 
         public void Run()
@@ -17,15 +26,27 @@ namespace Ecs.PlayerInput
             var leftClick = controls.Clicks.LeftClick.triggered;
             var rightClick = controls.Clicks.RightClick.triggered;
             var cursorPosition = controls.Clicks.CursorPosition.ReadValue<Vector2>();
-
+            var cameraView = (CameraView)camera.Get2(0).View;
             if (leftClick)
             {
-                Debug.Log($"Left Click");
+                currentClickable?.UnSelect();
+                var ray = cameraView.camera.ScreenPointToRay(cursorPosition);
+                if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, clickableMask))
+                {
+                    if (hit.collider.TryGetComponent(out IClickable clickable))
+                    {
+                        currentClickable = clickable;
+                        currentClickable.Select();
+                        currentClickable.Click();
+                    }
+                }
             }
 
             if (rightClick)
             {
-                Debug.Log($"Right Click");
+                currentClickable?.UnSelect();
+                currentClickable = null;
+                cameraView.CancelSelection();
             }
         }
     }
